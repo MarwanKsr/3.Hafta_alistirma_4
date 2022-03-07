dd4b46faf0f498c9ba307dcdda9e89d42a104742
Marwan <Marwan.kaseer1997@gmail.com>
2022-03-08 01:34:45 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace alistirma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Sayiyi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sonuc = 1;
+            for (int i=sayi; i>=1; i--)
+            {
+                sonuc *= i; 
+            }
+            Console.WriteLine("{0} sayisinin faktoriyelinin sonucu {1}",sayi,sonuc);
+        }
+    }
+}
